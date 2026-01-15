163bb827e8d62b6df5ee57c26085a6e007d2ad25
Devjack404 <juansabarofek@gmail.com>
2026-01-15 13:36:38 +0700

    menambah Tailwind,  membuat fitur edit task dan memberikan comment di baris code

--- app/app.vue
+++ app/app.vue
@@ -4,6 +4,8 @@
   const newTodo = ref('')// menyimpan teks input
   const todos = ref([])// Menyimpan daftar todo
 
+  //===============ADD TASK FUNCT===============
+
   function addTodo(){
     if(newTodo.value.trim() === '')//manambah nilai ke newTodo dan menghapus spasi
     return 
@@ -15,16 +17,33 @@
     newTodo.value = ''
   }
 
+  //==============DELETE FUNC=======================
+
   function deleteTodo(id){ //function hapus todo berdasar id
     todos.value = todos.value.filter(todo => todo.id !== id) 
+  }
+  
+
+  //===================EDIT FUNC======================
+
+  function editTask(id){ //function untuk mengedit berdasarkan id
+    const todo = todos.value.find(todo => todo.id === id) //ambil editTodo id yang sama dengan id yang dikirim ke fnctiom
+    
+    if(!todo) return //cek apakah editTodo tidak ada ? jika tdk ketemu funct berhenti
+
+    const newText = prompt("Edit Task :", todo.text) //munculkan input browser dengan judul "Edit Task"
+
+    if(newText !== null && newText.trim() !== "") { //pastikan tidak batal input dan trim() menghapus spasi sisa
+      todo.text = newText
+    }
   }
 </script>
 
 <template>
-  <div class="container">
-    <h1>Todo List</h1>
+  <div class="text-center p-5">
+    <h1 class="text-2xl">Todo List</h1>
 
-    <div class="input-group">
+    <div class="mb-3 bg-blue-200">
       <input 
       v-model="newTodo"
       type="text"
@@ -38,16 +57,9 @@
         <span :class="{done: todo.done}">
          {{ todo.text }}
         </span>
-        <button @click="deleteTodo(todo.id)">Delete</button>
+        <button class="mx-20" @click="deleteTodo(todo.id)">Delete</button>
+        <button @click="editTask(todo.id)">Edit</button>
       </li>
     </ul>
   </div>
 </template>
-
-<style scoped>
-  .container{
-    background-color : blue;
-    max-width : 400px;
-    margin : 50px auto;
-  }
-</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+import tailwindcss from "@tailwindcss/vite" 
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
@@ -7,5 +9,11 @@ export default defineNuxtConfig({
     timeline: {
       enabled: true
     }
+  },
+  css: ['@/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss()
+    ]
   }
 })
